8b4dc49d2415e88efeeda2b9ca5e2e029578e76b
Dariusz Kowalczyk <dariusz_kowalczykk@wp.pl>
2022-05-09 20:06:27 +0200

    Three laboratories were added.

--- 8_section/118_agregation.py
+++ 8_section/118_agregation.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from datetime import timedelta
 
 products = pd.read_csv(
     '../files_to_process/course-files/WA_Sales_Products_2012-14.csv'
@@ -43,4 +44,65 @@ print('------------------------------------------------')
 print('7')
 
 print(f"groups[['Revenue', 'Quantity']].mean().head() : \n{groups[['Revenue', 'Quantity']].mean().head()}")
-print('------------------------------------------------')
+print('**********************************************')
+
+
+# Laboratory
+
+df = pd.read_csv('../files_to_process/course-files/marathon_results_2016.csv', index_col='Bib',
+                 usecols=['Bib', '40K', 'Half', 'Pace', 'Age', 'M/F', 'Country', 'State', 'City']
+)
+
+print(f"df.head() : \n{df.head()}")
+print(f"df.info() : \n{df.info()}")
+print('----------')
+
+df['40K'] = pd.to_timedelta(df['40K'], errors='coerce', unit='H')
+df['Half'] = pd.to_timedelta(df['Half'], errors='coerce', unit='H')
+
+df.dropna(inplace=True)
+
+df['TotalSeconds'] = df['40K'].apply(lambda x: timedelta.total_seconds(x))
+df['HalfSeconds'] = df['Half'].apply(lambda x: timedelta.total_seconds(x))
+
+
+print("df['40K'] = df['40K'].apply(pd.to_timedelta)")
+print("df['Half'] = df['Half'].apply(pd.to_timedelta)")
+print("df['TotalSeconds'] = df['40K'].apply(lambda x: timedelta.total_seconds(x))")
+print("df['HalfSeconds'] = df['Half'].apply(lambda x: timedelta.total_seconds(x))")
+print(f"df.head() : \n{df.head()}")
+print('**********************************************')
+
+print('3')
+
+group_city = df.groupby(by='City')
+
+print("group_city = df.groupby(by='City')")
+print(f"group_city.head() : \n{group_city.head()}")
+print('**********************************************')
+
+print('4')
+
+print(f"group_city.agg(func='mean') : \n{group_city.agg(func='mean')}")
+print('**********************************************')
+
+print('5')
+
+print(f"group_city[['TotalSeconds','HalfSeconds']].mean() : \n{group_city[['TotalSeconds','HalfSeconds']].mean()}")
+print('**********************************************')
+
+print('6')
+
+print(f"group_city['TotalSeconds'].sum() : \n{group_city['TotalSeconds'].sum()}")
+print('**********************************************')
+
+group_age = df.groupby('Age')
+
+print("group_age = df.groupby('Age')")
+print(f"group_age.count() : \n{group_age.count()}")
+print('**********************************************')
+
+print('7')
+
+print(f"group_age['TotalSeconds'].count() : \n{group_age['TotalSeconds'].count()}")
+print('**********************************************')
--- 8_section/122_agg()_method.py
+++ 8_section/122_agg()_method.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from datetime import timedelta
 
 products = pd.read_csv(
     '../files_to_process/course-files/WA_Sales_Products_2012-14.csv'
@@ -45,4 +46,64 @@ print('7')
 
 print(r"groups.agg({'Revenue': ['sum', 'min', 'max'], 'Quantity': ['sum', 'min', 'max'], 'Gross margin': 'mean'}" +
       f"{groups.agg({'Revenue': ['sum', 'min', 'max'], 'Quantity': ['sum', 'min', 'max'], 'Gross margin': 'mean'})}")
-print('------------------------------------------------')
+print('**********************************************')
+
+#  Laboratory
+
+df = pd.read_csv('../files_to_process/course-files/marathon_results_2016.csv', index_col='Bib',
+                 usecols=['Bib', '40K', 'Half', 'Pace', 'Age', 'M/F', 'Country', 'State', 'City']
+                 )
+
+print(f"df.head() : \n{df.head()}")
+print(f"df.info() : \n{df.info()}")
+print('----------')
+
+df['40K'] = pd.to_timedelta(df['40K'], errors='coerce', unit='H')
+df['Half'] = pd.to_timedelta(df['Half'], errors='coerce', unit='H')
+
+df.dropna(inplace=True)
+
+df['TotalSeconds'] = df['40K'].apply(lambda x: timedelta.total_seconds(x))
+df['HalfSeconds'] = df['Half'].apply(lambda x: timedelta.total_seconds(x))
+
+print("df['40K'] = df['40K'].apply(pd.to_timedelta)")
+print("df['Half'] = df['Half'].apply(pd.to_timedelta)")
+print("df['TotalSeconds'] = df['40K'].apply(lambda x: timedelta.total_seconds(x))")
+print("df['HalfSeconds'] = df['Half'].apply(lambda x: timedelta.total_seconds(x))")
+print(f"df.head() : \n{df.head()}")
+print('**********************************************')
+
+print('3')
+
+sex_age = df.groupby(["M/F", "Age"])
+
+print("sex_age = df.groupby(['M/F', 'Age'])")
+print(f"sex_age.head() : \n{sex_age.head()}")
+print('**********************************************')
+
+print('4')
+
+print("sex_age.agg({'TotalSeconds': 'mean', 'HalfSeconds': 'mean'}) : "
+      f"\n{sex_age.agg({'TotalSeconds': 'mean', 'HalfSeconds': 'mean'})}")
+print('**********************************************')
+
+print('5')
+
+print("sex_age.agg({'TotalSeconds': 'sum', 'HalfSeconds': 'sum'}) : "
+      f"\n{sex_age.agg({'TotalSeconds': 'sum', 'HalfSeconds': 'sum'})}")
+print('**********************************************')
+
+print('6')
+
+print("sex_age.agg({'TotalSeconds': ['mean', 'sum'], 'HalfSeconds': ['mean', 'sum']}) : "
+      f"\n{sex_age.agg({'TotalSeconds': ['mean', 'sum'], 'HalfSeconds': ['mean', 'sum']})}")
+print('**********************************************')
+
+print('8')
+
+functions = ['mean', 'sum', 'count']
+
+print("functions = ['mean', 'sum', 'count']")
+print("sex_age.agg({'TotalSeconds': functions, 'HalfSeconds': functions}) : "
+      f"\n{sex_age.agg({'TotalSeconds': functions, 'HalfSeconds': functions})}")
+print('**********************************************')
